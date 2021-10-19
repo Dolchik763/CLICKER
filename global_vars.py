a5d366bc51f635020165cb2124a267fb020b988d
dolchik <dolchk763@gmail.com>
2021-10-19 18:38:40 +0300

    feat: add Menu , ficksed time crash
    add botton Menu, help,records

---
+++ global_vars.py
@@ -0,0 +1,4 @@
+from time import time
+
+cc = 0
+sc = time()
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from tkinter import *
 from time import time
+import global_vars
 from tkinter import messagebox as mb
 
+def show_help():
+    mb.showerror(message = "tap the button")
+def show_records():
+    records1 = ""
+    with open("clicker_records.txt", "r", ) as f:
+        record_list = f.readlines()
+        records1 = "".join(record_list)
+    mb.showerror(message = records1 )
+def show_about():
+    pass
 def cliker():
-    counter = 0
-    start = time()
+
     def check_record (name: str,score: int) -> None:
         with open("clicker_records.txt", "r+", ) as f:
             record_list = f.readlines()
 
-            new_record = [line.strip("\n").split(" ") for line in record_list]
+            new_record = [line.strip("\n").split(";") for line in record_list]
 
             for index, line_num in enumerate(new_record):
                 if score > int(line_num[2]):
@@ -22,15 +32,17 @@ def cliker():
             f.writelines(record_list)
             print(new_record)
     def click():
-        nonlocal counter,start
-        if time()-start<3:
-            counter += 1
-            lbl.config(text=f"tap\n{counter}")
+
+        if global_vars.cc == 0 :
+            global_vars.sc = time()
+        if time()-global_vars.sc<3:
+            global_vars.cc += 1
+            lbl.config(text=f"tap\n{global_vars.cc}")
         else:
-            confirm = mb.showerror(message=f"хватит жать кнопку\nваш результат{counter}")
+            confirm = mb.showerror(message=f"хватит жать кнопку\nваш результат{global_vars.cc}")
             if confirm == "ok":
-                start = time()
-                counter == 0
+                global_vars.sc = time()
+                global_vars.cc == 0
                 confirm ==0
                 lbl.config(text=f"tap\n{0}")
 
@@ -41,6 +53,22 @@ def cliker():
     root.resizable(False,False)
     icon_img = PhotoImage(file="164659.png")
     root.iconphoto(False,icon_img)
+    """
+    создаём понель меню под заголовком
+    """
+    mainmenu = Menu(root)
+    root.config(menu = mainmenu)
+     #mainmenu.add_command(label = "info",command = root.)
+     #mainmenu.add_command(label="Exit",command = root.destroy)
+    filemenu = Menu(mainmenu,tearoff = 0)
+    filemenu.add_command(label="Exit", command=root.destroy)
+    refmenu = Menu(mainmenu,tearoff = 1)
+    refmenu.add_command(label="Help", command=show_help)
+    refmenu.add_command(label="records", command=show_records)
+    refmenu.add_command(label="about", command=show_about)
+
+    mainmenu.add_cascade(label = "file", menu=filemenu)
+    mainmenu.add_cascade(label="referc", menu=refmenu)
 
     btn = Button(text = "tap"
                  ,font = ("Comic Sans",15),
